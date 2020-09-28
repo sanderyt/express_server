e3c4224bd6e0cb060ab46136b646faf37628c0b4
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-09-28 19:50:01 +0200

    post req for user

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("MongoDB was connected");
+  }
+);
 
-const members = [
-  { id: 1, name: "Sander", residence: "Amsterdam" },
-  { id: 2, name: "Stijn", residence: "Amsterdam" },
-  { id: 3, name: "Max", residence: "Rotterdam" }
-];
+//Import routes
+const userRoutes = require("./routes/users");
+
+app.use("/users", userRoutes);
 
 app.use(function(req, res, next) {
-  // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-
-  // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
   next();
 });
 
@@ -41,13 +37,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Express Server");
 });
 
-//Get all users
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-//Get single user
-app.get("/api/members/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  res.json(members.filter(member => member.id === id));
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
