fba61edec9e23b6c6f8ac6229740dcb9995134b3
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-09-28 21:24:35 +0200

    removed some code

--- routes/users.js
+++ routes/users.js
@@ -16,22 +16,8 @@ router.post("/", (req, res) => {
     })
     .catch(error => {
       res.json({ message: error });
+      res.status(500).statusMessage;
     });
-});
-
-router.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  next();
 });
 
 module.exports = router;
