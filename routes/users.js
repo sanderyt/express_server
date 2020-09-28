06cb797c8fa23d4c5833f9804c1df3bb7b5da6d8
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-09-28 20:48:56 +0200

    added cors policy for users endpoint

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,19 @@ router.post("/", (req, res) => {
     });
 });
 
+router.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 module.exports = router;
