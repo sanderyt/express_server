3b645c300c444f0b01bda815eb82e9d6b1d88267
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-09-30 18:13:38 +0200

    made the register endpoint

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-const cors = require("cors");
-const userRoutes = require("./routes/users");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
-require("dotenv").config();
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+//Connect to DB
 mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -17,12 +18,12 @@ mongoose.connect(
 );
 
 //Middleware
-app.use(cors());
-
 app.use(express.json());
 
-app.use("/users", userRoutes);
+//Route middlewares
+app.use("/api/user/", authRoute);
 
+//Server
 app.get("/", (req, res) => {
   res.send("Welcome to my Express Server");
 });
